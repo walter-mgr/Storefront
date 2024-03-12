d962a3d12638512f9b0be79e902d147e872ddfd9
Valeriy Borzak <valeraborzak@gmail.com>
2024-03-12 19:16:42 +0100

    Validate the Product ID from 0 or negative

--- store/serializers.py
+++ store/serializers.py
@@ -145,6 +145,11 @@ class CartSerializer(serializers.ModelSerializer):
 class AddCartItemSerializer(serializers.ModelSerializer):
     product_id = serializers.IntegerField()
 
+    def validate_product_id(self, value):
+        if not Product.objects.filter(pk=value).exists():
+            raise serializers.ValidationError("Product does not exist")
+        return value
+
     def save(self, **kwargs):
         cart_id = self.context["cart_id"]
         product_id = self.validated_data["product_id"]
