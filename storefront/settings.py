9d0812e707b3567618f1479c372c368d755fd8b2
Valeriy Borzak <valeraborzak@gmail.com>
2024-03-19 18:09:25 +0100

    Connect JSON Web Token Authentication, fix the bug in the settings

--- storefront/settings.py
+++ storefront/settings.py
@@ -71,7 +71,9 @@ REST_FRAMEWORK = {
     "COERCE_DECIMAL_TO_STRING": False,
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 10,
-    "DEFAULT_AUTHENTICATION_CLASSES": "rest_framework_simplejwt.authentication.JWTAuthentication",
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework.authentication.TokenAuthentication",
+    ),
 }
 
 SIMPLE_JWT = {
