513788c1dd71791ff596c2b866c2176fb8019e52
Valeriy Borzak <valeraborzak@gmail.com>
2024-03-25 21:03:13 +0100

    Set a 1 day lifetime 'Access' token

--- storefront/settings.py
+++ storefront/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 from config import SQL_PASSWORD
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -80,6 +81,7 @@ DJOSER = {"SERIALIZERS": {"user_create": "core.serializers.UserCreateSerializer"
 
 SIMPLE_JWT = {
     "AUTH_HEADER_TYPES": ("JWT",),
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
 }
 
 ROOT_URLCONF = "storefront.urls"
