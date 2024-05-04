64e7d3fb23b4c634a680ad9259458f2c55890b4f
Valeriy Borzak <valeraborzak@gmail.com>
2024-05-04 20:15:16 +0200

    Validate uploaded images

--- store/models.py
+++ store/models.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from django.conf import settings
 from django.core.validators import RegexValidator, MinValueValidator
 from uuid import uuid4
+from .validators import validate_file_size
 
 
 class Promotion(models.Model):
@@ -42,7 +43,7 @@ class ProductImage(models.Model):
     product = models.ForeignKey(
         Product, on_delete=models.CASCADE, related_name="images"
     )
-    image = models.ImageField(upload_to="store/images")
+    image = models.ImageField(upload_to="store/images", validators=[validate_file_size])
 
 
 class Customer(models.Model):
