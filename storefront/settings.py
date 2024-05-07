017e3a006772227160a230d6fd9fe036aed0cc70
Valeriy Borzak <valeraborzak@gmail.com>
2024-05-07 18:53:05 +0200

    Enabling CORS

--- storefront/settings.py
+++ storefront/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.sessions",
     "django.contrib.staticfiles",
+    "corsheaders",
     "debug_toolbar",
     "django_filters",
     "rest_framework",
@@ -59,7 +60,13 @@ INTERNAL_IPS = [
     # ...
 ]
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:8001",
+    "http://127.0.0.1:8001",
+]
+
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "debug_toolbar.middleware.DebugToolbarMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
