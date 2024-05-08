c6abc2ae682cddbf4e359d078f93af85a63af6b8
Valeriy Borzak <valeraborzak@gmail.com>
2024-05-08 16:40:24 +0200

    Configuring the email backend

--- storefront/settings.py
+++ storefront/settings.py
@@ -179,3 +179,14 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+# https://docs.djangoproject.com/en/5.0/topics/email/#obtaining-an-instance-of-an-email-backend
+# https://docs.djangoproject.com/en/5.0/ref/settings/#default-from-email
+# EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "localhost"
+EMAIL_HOST_USER = ""
+EMAIL_HOST_PASSWORD = ""
+EMAIL_PORT = 5000
+DEFAULT_FROM_EMAIL = "from@dev.com"
