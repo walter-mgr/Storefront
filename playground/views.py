724c521d36692c48a36248a45fd7aace41b3df03
Valeriy Borzak <valeraborzak@gmail.com>
2024-05-13 10:42:20 +0200

    Send email to admin

--- playground/views.py
+++ playground/views.py
@@ -1,4 +1,4 @@
-from django.core.mail import send_mail, BadHeaderError
+from django.core.mail import send_mail, mail_admins, BadHeaderError
 from django.shortcuts import render
 from django.http import HttpResponse
 from store.models import Customer
@@ -6,11 +6,25 @@ from store.models import Customer
 
 # Create your views here.
 
-
+"""
 def say_hello(request):
     try:
         send_mail(
             "subject", "message", "info_1@dev.com", ["bob_1@dev.com", "bob_2@dev.com"]
+        )
+    except BadHeaderError:
+        pass
+
+    return render(request, "hello.html", {"name": "Walter"})
+"""
+
+
+def say_hello(request):
+    try:
+        mail_admins(
+            "subject",
+            "new message",
+            html_message="<H1>Email for admin was successfully sent</H1>",
         )
     except BadHeaderError:
         pass
--- storefront/settings.py
+++ storefront/settings.py
@@ -190,3 +190,5 @@ EMAIL_HOST_USER = ""
 EMAIL_HOST_PASSWORD = ""
 EMAIL_PORT = 2525
 DEFAULT_FROM_EMAIL = "from@dev.com"
+
+ADMINS = [("Admin", "fake.admin@dev.com")]
