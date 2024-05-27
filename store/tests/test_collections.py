bb0ea734c18b25b45e79075548c1d90892bd9e4f
Valeriy Borzak <valeraborzak@gmail.com>
2024-05-27 19:21:26 +0200

    Two tests for validating data, multiple assertions

--- store/tests/test_collections.py
+++ store/tests/test_collections.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from rest_framework import status
 from rest_framework.test import APIClient
 import pytest
@@ -14,3 +15,29 @@ class TestCreateCollection:
 
         # Assert
         assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+    def test_if_user_is_not_admin_returns_403(self):
+
+        client = APIClient()
+        client.force_authenticate(user={})
+        response = client.post("/store/collections/", {"title": "a"})
+
+        assert response.status_code == status.HTTP_403_FORBIDDEN
+
+    def test_if_data_is_not_valid_returns_400(self):
+
+        client = APIClient()
+        client.force_authenticate(user=User(is_staff=True))
+        response = client.post("/store/collections/", {"title": ""})
+
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+        assert response.data["title"] is not None
+
+    def test_if_data_is_valid_returns_201(self):
+
+        client = APIClient()
+        client.force_authenticate(user=User(is_staff=True))
+        response = client.post("/store/collections/", {"title": "a"})
+
+        assert response.status_code == status.HTTP_201_CREATED
+        assert response.data["id"] > 0
