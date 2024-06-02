02f3ea13daaa77a3cf9a7cd9a5f16710bac3ae0c
Valeriy Borzak <valeraborzak@gmail.com>
2024-06-02 08:06:25 +0200

    Writing a task script

--- locustfiles/browse_products.py
+++ locustfiles/browse_products.py
@@ -1,4 +1,4 @@
-from locust import HttpUser, task
+from locust import HttpUser, task, between
 from random import randint
 
 # Viewing products
@@ -7,20 +7,32 @@ from random import randint
 
 
 class WebsiteUser(HttpUser):
-    @task
+    wait_time = between(1, 5)
+
+    @task(2)
     def view_products(self):
         collection_id = randint(2, 6)
 
         self.client.get(
-            f"/store/products/?collection_id={collection_id}/", name="/store/products/"
+            f"/store/products/?collection_id={collection_id}", name="/store/products"
         )
 
-    @task
+    @task(4)
     def view_product(self):
         product_id = randint(1, 1000)
 
-        self.client.get(f"/store/products/{product_id}/", name="/store/products/:id")
+        self.client.get(f"/store/products/{product_id}", name="/store/products/:id")
 
-    @task
+    @task(1)
     def add_product_to_cart(self):
-        pass
+        product_id = randint(1, 10)
+        self.client.post(
+            f"/store/carts/{self.cart_id}/items/",
+            name="/store/carts/items",
+            json={"product_id": product_id, "quantity": 1},
+        )
+
+    def on_start(self) -> None:
+        response = self.client.post("/store/carts/")
+        result = response.json()
+        self.cart_id = result["id"]
