27709aa99ecfce709a519c71d725c4145280b953
Valeriy Borzak <valeraborzak@gmail.com>
2024-06-23 10:06:24 +0200

    Trying to fix favicon.ico

--- storefront/urls.py
+++ storefront/urls.py
@@ -19,6 +19,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
+
+
 import debug_toolbar
 
 
@@ -26,6 +29,7 @@ admin.site.site_header = "Storefront Admin"
 admin.site.index_title = "Admin"
 
 urlpatterns = [
+    path("favicon.ico", RedirectView.as_view(url="/static/images/favicon.ico")),
     path("", include("core.urls")),
     path("admin/", admin.site.urls),
     path("store/", include("store.urls")),
