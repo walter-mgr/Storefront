923b3a687fb505f95c46557d0b4b22eacf566eac
Valeriy Borzak <valeraborzak@gmail.com>
2024-07-05 14:27:15 +0200

    Server.py change port to 8080

--- server.py
+++ server.py
@@ -2,4 +2,4 @@ from waitress import serve
 from storefront.wsgi import application
 
 if __name__ == "__main__":
-    serve(application, host="127.0.0.1", port="8000")
+    serve(application, host="127.0.0.1", port="8080")
